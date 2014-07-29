c2f41538098b131f9e73738887046e3451ddd1f9
Raul Pino <rpinocastellano@groupon.com>
2014-07-28 22:42:25 -0400

    version using storejs and listing the books

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -2,30 +2,38 @@
 
   this.Mil1App.controller('HomeCtrl', function($scope) {
   });
-  this.Mil1App.controller('CalculatorCtrl', function($scope, $ionicPopup, BooksBeforeDeath) {
+  this.Mil1App.controller('CalculatorCtrl', function($scope, $state, $ionicPopup, BooksBeforeDeath) {
     $scope.user = {};
     $scope.isPendingRequest = false;
 
     $scope.onCalculateBooks = function(){
       $scope.isPendingRequest = true;
 
-      BooksBeforeDeath.get({
-        data: { age: $scope.user.age, books_per_year: $scope.user.booksPerYear},
-        onSuccess: function(data, status, headers, config) {
-          $scope.isPendingRequest = false;
+      BooksBeforeDeath.get({ data: { age: $scope.user.age, books_per_year: $scope.user.booksPerYear},
+        success: function(data, status, headers, config) {
+          store.set('user', data);
+          $state.go('tab.books');
         },
-        onError: function(data, status, headers, config) {
-          $scope.isPendingRequest = false;
+        error: function(data, status, headers, config) {
           $ionicPopup.alert({
             title: 'Error de conexion',
             template: 'Algo salio mal al comunicarse con el servidor, intentalo de nuevo.'
           });
+        },
+        finally: function(){
+          $scope.isPendingRequest = false;
         }
       });
     };
   });
 
-  this.Mil1App.controller('BooksCtrl', function($scope) {
+  this.Mil1App.controller('BooksCtrl', function($scope, $state) {
+    $scope.user = store.get('user');
+
+    if (!$scope.user){
+      $state.go('tab.calculator');
+    }
+
   });
 
   this.Mil1App.controller('AboutCtrl', function($scope) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,17 +2,17 @@
 
   this.Mil1App.controller('HomeCtrl', function($scope) {
   });
-  this.Mil1App.controller('CalculatorCtrl', function($scope, $ionicPopup, BooksBeforeDeath) {
+  this.Mil1App.controller('CalculatorCtrl', function($scope, $state, $ionicPopup, BooksBeforeDeath) {
     $scope.user = {};
     $scope.isPendingRequest = false;
 
     $scope.onCalculateBooks = function(){
       $scope.isPendingRequest = true;
 
-      BooksBeforeDeath.get({
-        data: { age: $scope.user.age, books_per_year: $scope.user.booksPerYear},
+      BooksBeforeDeath.get({ data: { age: $scope.user.age, books_per_year: $scope.user.booksPerYear},
         success: function(data, status, headers, config) {
-          
+          store.set('user', data);
+          $state.go('tab.books');
         },
         error: function(data, status, headers, config) {
           $ionicPopup.alert({
@@ -27,7 +27,13 @@
     };
   });
 
-  this.Mil1App.controller('BooksCtrl', function($scope) {
+  this.Mil1App.controller('BooksCtrl', function($scope, $state) {
+    $scope.user = store.get('user');
+
+    if (!$scope.user){
+      $state.go('tab.calculator');
+    }
+
   });
 
   this.Mil1App.controller('AboutCtrl', function($scope) {
